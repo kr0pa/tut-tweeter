6105f673bdd0748b4fb4d5bc529f0b1db22a523b
Piotr Górski <piotrkropagorski@gmail.com>
2024-01-05 14:59:12 +0100

    add model Profile

--- social/admin.py
+++ social/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group, User
 
 # Register your models here.
+admin.site.unregister(Group)
+
+class UserAdmin(admin.ModelAdmin):
+        model = User
+        fields = ['username']
+
+admin.site.unregister(User)       
+admin.site.register(User, UserAdmin)
